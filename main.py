76b5aaaf9e622c2934e3db7cdc34599ceb4ab438
tpetersen4 <ttp10@pitt.edu>
2024-03-20 23:22:40 -0400

    Added prompt and API call to gemini to produce cover letter

--- main.py
+++ main.py
@@ -9,9 +9,15 @@ from starlette.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from dotenv import load_dotenv
 from linkedin_api import Linkedin
+import google.generativeai as genai
 
 # Imports the Cloud Logging client library
 import google.cloud.logging
+
+# define key and model
+genai.configure(api_key='')
+model = genai.GenerativeModel('gemini-pro')
+
 
 if "GCP_PROJECT" in os.environ:
     # Instantiates a client
@@ -44,6 +50,30 @@ app.add_middleware(
     allow_methods=["*"],  # Allow all HTTP methods, or specify specific methods
     allow_headers=["*"],  # Allow all headers, or specify specific headers
 )
+
+def generate_prompt(user_details, job_description):
+    prompt_pt1 = """
+    Please write a cover letter. Some of the information I provide may be extraneous, such as playing sports, or
+    running recreation clubs, please avoid using this information in the cover letter. Try to only
+    include only the professional information or information that relates to the company or job role (i.e. job experience, skills, school). 
+    Please start with a header. Than write about 4-5 paragraphs each being 3-4 sentences long. Include and introduction, conclusion and sign off.
+    I am going to provide you with my information as well as a short description about the role I'm pursuing and the company.
+    Here is my personal information information:
+    """
+    prompt_pt2 = """
+    Here is a description of the role and company I would like to write the cover letter for:
+    """
+    prompt = prompt_pt1 + user_details + prompt_pt2 + job_description
+    print(prompt + "\n\n\n")
+
+    return prompt
+
+
+def user_details_to_string(user_info):
+    details_string = ""
+    for key, value in user_info.items():
+        details_string += f"{key}: {value}\n"
+    return details_string
 
 
 def get_generic_cover_letter():
@@ -120,7 +150,19 @@ async def generate(context: Context):
                     userDetails["education2"]=profile["education"][1]["fieldOfStudy"]+"at "+profile["education"][1]["schoolName"]
 
     print("user_details---\n",userDetails.keys(), userDetails)
-    # Send user data + job description to Vertex AI (Trevor)
+
+    # Convert the details dict into a string so it can be passed passed to gemini
+    details_as_string = user_details_to_string(userDetails)
+
+    # Generate prompt and make call to gemini
+    # TODO: Include information about job role that user is seeking
+    prompt = generate_prompt(details_as_string, "Software Engineer at Tesla")
+    response = model.generate_content(prompt)
+
+    cover_letter = response.text.replace("*", "")
+    print("\ncover_letter---\n")
+
+    # TODO: stream out cover letter
 
     # Steam response from Vertex AI (Trevor)
     logging.info(context)
