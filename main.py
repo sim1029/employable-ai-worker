316ca8b765e99a38447b88faf4bc47a3694f5c92
Simon Schueller <sps67@pitt.edu>
2024-03-25 17:21:47 -0400

    alter endpoint to handle resume uploads instead of linkedin URLs

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ import time
 from fastapi.responses import JSONResponse
 import openai
 import logging
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, UploadFile
 from starlette.responses import StreamingResponse
 from starlette.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from dotenv import load_dotenv
 from linkedin_api import Linkedin
 import google.generativeai as genai
+import docx2txt
+from PyPDF2 import PdfReader
 
 # Imports the Cloud Logging client library
 import google.cloud.logging
@@ -34,11 +36,6 @@ genai.configure(api_key=GEMINI_AI_API_KEY)
 model = genai.GenerativeModel("gemini-pro")
 
 
-class Context(BaseModel):
-    linkedinURL: str
-    jobpostDesc: str
-
-
 app = FastAPI()
 
 
@@ -58,7 +55,7 @@ def generate_prompt(user_details, job_description):
     include only the professional information or information that relates to the company or job role (i.e. job experience, skills, school). 
     Please start with a header. Than write about 4-5 paragraphs each being 3-4 sentences long. Include and introduction, conclusion and sign off.
     I am going to provide you with my information as well as a short description about the role I'm pursuing and the company.
-    Here is my personal information information:
+    Here is my personal information information (FOCUS ON EXTRACTING THE FIRST AND LAST NAME AT A MINIMUM):
     """
     prompt_pt2 = """
     Here is a description of the role and company I would like to write the cover letter for:
@@ -68,11 +65,16 @@ def generate_prompt(user_details, job_description):
     return prompt
 
 
-def user_details_to_string(user_info):
-    details_string = ""
-    for key, value in user_info.items():
-        details_string += f"{key}: {value}\n"
-    return details_string
+def check_file_format(filename):
+    extension = filename.lower().split(".")[-1]
+    if extension == "pdf":
+        return "pdf"
+    elif extension == "docx":
+        return "docx"
+    elif extension == "txt":
+        return "txt"
+    else:
+        return "unknown"
 
 
 @app.get("/")
@@ -81,95 +83,31 @@ async def root():
 
 
 @app.post("/generate")
-async def generate(context: Context):
-    # Get linkedin DATA with (Harry) code
-    try:
-        print(LINKEDIN_ACC_EMAIL, LINKEDIN_ACC_PASS)
-        api = Linkedin(LINKEDIN_ACC_EMAIL, LINKEDIN_ACC_PASS)
-        split_username = context.linkedinURL.split("/")
-        if split_username[-1] == "":
-            username = split_username[-2]
-        else:
-            username = split_username[-1]
-        profile = api.get_profile(username)
-        userDetails = {}
-        if "summary" in profile:
-            userDetails["summary"] = profile["summary"]
-        if profile["experience"] != []:
-            if (
-                "companyName" in profile["experience"][0]
-                and "description" in profile["experience"][0]
-            ):
-                userDetails["experience1"] = (
-                    profile["experience"][0]["companyName"]
-                    + "doing "
-                    + profile["experience"][0]["description"]
-                )
-            if len(profile["experience"]) >= 2:
-                if (
-                    "companyName" in profile["experience"][1]
-                    and "description" in profile["experience"][1]
-                ):
-                    userDetails["experience2"] = (
-                        profile["experience"][1]["companyName"]
-                        + "doing "
-                        + profile["experience"][1]["description"]
-                    )
-        if profile["projects"] != []:
-            if (
-                "title" in profile["projects"][0]
-                and "description" in profile["projects"][0]
-            ):
-                userDetails["project1"] = (
-                    profile["projects"][0]["title"]
-                    + ": "
-                    + profile["projects"][0]["description"]
-                )
-        if profile["education"] != []:
-            if (
-                "fieldOfStudy" in profile["education"][0]
-                and "schoolName" in profile["education"][0]
-            ):
-                userDetails["education1"] = (
-                    profile["education"][0]["fieldOfStudy"]
-                    + "at "
-                    + profile["education"][0]["schoolName"]
-                )
-                if len(profile["education"]) >= 2:
-                    if (
-                        "fieldOfStudy" in profile["education"][1]
-                        and "schoolName" in profile["education"][1]
-                    ):
-                        userDetails["education2"] = (
-                            profile["education"][1]["fieldOfStudy"]
-                            + "at "
-                            + profile["education"][1]["schoolName"]
-                        )
-    except Exception as e:
-        print("Error getting linkedin data")
-        print(e)
-        userDetails = {
-            "summary": "",
-            "experience1": "",
-            "experience2": "",
-            "project1": "",
-            "education1": "",
-            "education2": "",
-        }
+async def generate(resumeFile: UploadFile, jobpostDesc: str = Form(...)):
+    print("Job Posting Description:", jobpostDesc)
+    print("Resume File Name:", resumeFile.filename)
 
-    print(userDetails)
-    # Convert the details dict into a string so it can be passed passed to gemini
-    details_as_string = user_details_to_string(userDetails)
+    resume_text = ""
+    file_type = check_file_format(resumeFile.filename)
+    if file_type == "pdf":
+        pdf_reader = PdfReader(resumeFile.file)
+        for page_number in range(len(pdf_reader.pages)):
+            page = pdf_reader.pages[page_number]
+            resume_text += page.extract_text()
+    elif file_type == "docx":
+        resume_text = docx2txt.process(resumeFile.file)
+    else:
+        print("FILE TYPE NOT SUPPORTED")
+
+    print(resume_text)
 
     # Generate prompt and make call to gemini
     # TODO: Include information about job role that user is seeking
     prompt = generate_prompt(
-        details_as_string,
-        context.jobpostDesc,
+        resume_text,
+        jobpostDesc,
     )
     responses = model.generate_content(prompt, stream=True)
-
-    logging.info(context)
 
     return StreamingResponse(
         (response.text for response in responses),
